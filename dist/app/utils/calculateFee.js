3555e9c188bd22d4582310c733b44655e5b452bb
nayeem-miah <nayeem5113a@gmail.com>
2025-09-24 09:16:07 +0600

    Remove dist and .vscode from repo

--- dist/app/utils/calculateFee.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateFrr = void 0;
-const calculateFrr = (width) => {
-    const widthPerKg = 2;
-    return Math.ceil(width) * widthPerKg;
-};
-exports.calculateFrr = calculateFrr;
